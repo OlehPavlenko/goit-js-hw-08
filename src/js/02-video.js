058f213ff2e7afef2a1298015a3d202830570c2c
OlehPavlenko <pavlenko.zevs@gmail.com>
2022-06-16 21:14:32 +0300

    fix bug

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,9 +4,20 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const storageKey = 'videoplayer-current-time';
+const key = localStorage.getItem(storageKey);
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then().catch();
+console.log(key)
 
-player.on('timeupdate', throttle((data) => {
-    localStorage.setItem("videoplayer-current-time", data.seconds);
+if (key) {
+  player.setCurrentTime(JSON.parse(key));
+}
+
+player.on('timeupdate', throttle(data => {
+  localStorage.setItem(storageKey, JSON.stringify(data.seconds));
 }, 1000));
+// player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then().catch();
+
+// player.on('timeupdate', throttle((data) => {
+//     localStorage.setItem("videoplayer-current-time", data.seconds);
+// }, 1000));
